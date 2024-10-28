0b71a763440b4f354a516b535059831b8018affc
Júlio Freitas <julio.dev.freitas@gmail.com>
2024-10-28 19:21:42 -0300

    Resolução

---
+++ problems/1037.js
@@ -0,0 +1,15 @@
+export function problem(lines){
+let numero = parseFloat(lines[0]);
+
+if ( numero >= 0 && numero <=25 ){
+    console.log(`Intervalo [0,25]`);
+} else if ( numero > 25 && numero <=50 ){
+    console.log(`Intervalo (25,50]`);
+} else if ( numero > 50 && numero <=75 ){
+    console.log(`Intervalo (50,75]`);
+} else if ( numero > 75 && numero <=100 ){
+    console.log(`Intervalo (75,100]`);
+} else {
+    console.log("Fora de intervalo");
+}
+}
