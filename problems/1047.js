55c571c8817fc123ee6ef942147cd87e220b6d5f
Júlio Freitas <julio.dev.freitas@gmail.com>
2024-10-31 19:14:01 -0300

    Resolução

--- problems/1047.js
+++ problems/1047.js
@@ -1,31 +1,26 @@
 export function problem(lines){
     let [horaI, minutoI, horaF, minutoF] = lines[0].split(" ").map(Number);
-    let tmpJogoH = 0;
-    let tmpJogoM = 0;
 
-    if (horaI < horaF) {
-        tmpJogoH += (horaF - horaI);
-            if (minutoI < minutoF){
-                tmpJogoM += (minutoF - minutoI);
-            } else {
-                tmpJogoH -= 1;
-                tmpJogoM += (60-minutoI) + minutoF;
-            }
+    const transformarMinutos = (hora, minuto) => {
+        return hora*60 + minuto;
+    }
+
+    let dia = transformarMinutos(24,0);
+    let inicioMin = transformarMinutos(horaI, minutoI);
+    let finalMin = transformarMinutos(horaF, minutoF);
+    let durJogoMin = 0;
+
+    if (inicioMin < finalMin){
+        durJogoMin += (finalMin - inicioMin);
     } else {
-        tmpJogoH +=(24 - horaI) + horaF;
-            if (minutoI < minutoF){
-                    if (horaF === horaI) { tmpJogoH = 0};
-                tmpJogoM += (minutoF - minutoI);
-            }  else {
-                tmpJogoH -= 1;
-                tmpJogoM += (60-minutoI) + minutoF;
-            }
+        durJogoMin += (dia - inicioMin) + finalMin;
+    }
+    
+    const transformarHora = (minutosTotais) => {
+        return Math.floor(durJogoMin/60);
     }
 
-    if (minutoI === minutoF){
-        tmpJogoH +=1;
-        tmpJogoM = 0;
-    }
-
-    console.log(`O JOGO DUROU ${tmpJogoH} HORA(S) E ${tmpJogoM} MINUTO(S)`)
+    let horasJogo = transformarHora(durJogoMin); 
+    let minutosJogo = durJogoMin%60;
+    console.log(`O JOGO DUROU ${horasJogo} HORA(S) E ${minutosJogo} MINUTO(S)`)
 }
